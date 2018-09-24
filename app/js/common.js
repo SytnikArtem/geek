1bea9b5c7f9853ca239f55f555a22ce4f7371a17
SytnikArtem <artemasitnik@gmail.com>
2018-09-24 18:42:06 +0300

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+    $('.menu-left-item').hover(function() {
+        var index = $(this).index();
+        console.log(index);
+        // $('.menu-right-item').removeClass('active');
+        $('.menu-right-img').eq(index).addClass('active').siblings().removeClass('active');
+    }, function(){
+    });
+  $('.slider-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nav: false,
+    dots: true,
+    fade: true
+    });
+  $('.categories-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nav: true,
+    dots: false
+  });
 });
