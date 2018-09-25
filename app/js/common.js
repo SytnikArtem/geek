c628dcef0efc357fec8f0e5088418037e86faa0a
SytnikArtem <artemasitnik@gmail.com>
2018-09-25 18:06:39 +0300

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -17,6 +17,23 @@ $(document).ready(function() {
     slidesToShow: 1,
     slidesToScroll: 1,
     nav: true,
-    dots: false
+    dots: true
+  });
+  var height = $('.mobile-left-list.active').height();
+  $('.mobile-left-slide').css('height', height);
+  $('.mobile-left').click(function() {
+    $('.mobile-left-overlay').addClass('active');
+  });
+  $('.mobile-left-item_enter').click(function(){
+    $('.mobile-left-list').removeClass('active');
+    $('.mobile-left-list').eq(1).addClass('active');
+    height = $('.mobile-left-list.active').height();
+    $('.mobile-left-slide').css('height', height);
+  });
+  $('.mobile-left-item_first').click(function(){
+    $(this).closest('.mobile-left-list').removeClass('active')
+    $('.mobile-left-list').eq(0).addClass('active');
+    height = $('.mobile-left-list.active').height();
+    $('.mobile-left-slide').css('height', height);
   });
 });
